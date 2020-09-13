9501bae29ee4295b474d3fccd0c4659c4cfd78a5
devendr4 <angel.emo.altuve@gmail.com>
2020-09-13 19:57:16 -0400

    react router

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
-const Footer = (props) => {
+import { Link } from "react-router-dom";
+const Footer = () => {
   return (
     <div className="footer">
       <div className="container">
@@ -8,16 +9,16 @@ const Footer = (props) => {
             <h5>Links</h5>
             <ul className="list-unstyled">
               <li>
-                <a href="#">Home</a>
+                <Link to="/home">Home</Link>
               </li>
               <li>
-                <a href="#">About</a>
+                <Link to="/aboutus">About Us</Link>
               </li>
               <li>
-                <a href="#">Menu</a>
+                <Link to="/menu">Menu</Link>
               </li>
               <li>
-                <a href="contactus.html">Contact</a>
+                <Link to="/contactus">Contact Us</Link>
               </li>
             </ul>
           </div>
