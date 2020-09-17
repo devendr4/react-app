f7fbf6b12a43b739fafc9e4461ef6df39c74a82b
devendr4 <angel.emo.altuve@gmail.com>
2020-09-17 13:45:02 -0400

    build

---
+++ public/precache-manifest.cf5eef2f3badf78c2e9fc7a2c0924855.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07369413f1a2cc6206c65d4b47d9b11c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2847ccc2f6e6132b2172",
+    "url": "/static/css/2.3a9061f6.chunk.css"
+  },
+  {
+    "revision": "4c350f8136c6854bdc9d",
+    "url": "/static/css/main.22041fe0.chunk.css"
+  },
+  {
+    "revision": "2847ccc2f6e6132b2172",
+    "url": "/static/js/2.39a9b037.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.39a9b037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c350f8136c6854bdc9d",
+    "url": "/static/js/main.5d40966b.chunk.js"
+  },
+  {
+    "revision": "5387050906b8ed53cb42",
+    "url": "/static/js/runtime-main.ec2d9410.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
